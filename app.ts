59f1b5e9dedf247dde85e8773291481ee84d68da
carlos <cbcarlos07@gmail.com>
2018-04-30 21:39:38 -0400

    Code

--- app.ts
+++ app.ts
@@ -1,3 +1,7 @@
+
+
+
+/*
 import { SpaceCraft } from "./base-ships";
 import { MilleniumFalcon } from "./starfighters";
 import * as _ from 'lodash'
@@ -17,4 +21,36 @@ interface ContainerShip{
 
 let goodForTheJob  = ( ship : ContainerShip ) => ship.cargoContainers > 2
 
-console.log( `Is Falcon good for the job? ${goodForTheJob( falcon ) ? 'yes' : 'no'}` )
+console.log( `Is Falcon good for the job? ${goodForTheJob( falcon ) ? 'yes' : 'no'}` )*/
+/*
+
+let message = "Helpe me, Obi-Wan Kenobi. You're my only hope!"
+console.log( message )
+
+let episode: number = 4
+//episode = '4'
+console.log( "This episode: "+episode )
+
+episode = episode + 1
+console.log( "Next episode: "+episode )
+
+let favoriteDroid : string // tipo any quando não informa o tipo na declaração
+//let favoriteDroid // tipo any quando não informa o tipo na declaração
+favoriteDroid = 'BB-8'
+//favoriteDroid = 10
+console.log( 'My favorite droid is: '+favoriteDroid )
+
+let isEnoughToBeatMF = function ( parsecs : number ) : boolean {
+    return parsecs < 12
+}
+let distance = 14
+console.log( `is ${distance} parsecs enough to beat Milenium Falcon? ${isEnoughToBeatMF(distance) ? 'YES': 'NO' } `)
+
+let call = (name: string) => console.log( `Do you copy, ${name}?` )
+call( 'R2' )
+
+function inc ( speed: number, inc: number = 1 ) : number{
+    return speed + inc
+}
+console.log( `inc (5, 1) = ${inc(5,1)}` )
+console.log( `inc (5) = ${inc(5)}` )*/
--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,46 @@
-"use strict";
-exports.__esModule = true;
-var base_ships_1 = require("./base-ships");
-var starfighters_1 = require("./starfighters");
-var _ = require("lodash");
-console.log(_.pad("Typescript examples", 40, "="));
-var ship = new base_ships_1.SpaceCraft('hiperdrive');
-ship.jumpIntoMyperspace();
-var falcon = new starfighters_1.MilleniumFalcon();
-falcon.jumpIntoMyperspace();
-var goodForTheJob = function (ship) { return ship.cargoContainers > 2; };
-console.log("Is Falcon good for the job? " + (goodForTheJob(falcon) ? 'yes' : 'no'));
+/*
+import { SpaceCraft } from "./base-ships";
+import { MilleniumFalcon } from "./starfighters";
+import * as _ from 'lodash'
+
+console.log( _.pad("Typescript examples", 40, "=") )
+
+let ship = new SpaceCraft('hiperdrive')
+ship.jumpIntoMyperspace()
+
+
+let falcon  = new MilleniumFalcon()
+falcon.jumpIntoMyperspace()
+
+interface ContainerShip{
+    cargoContainers : number
+}
+
+let goodForTheJob  = ( ship : ContainerShip ) => ship.cargoContainers > 2
+
+console.log( `Is Falcon good for the job? ${goodForTheJob( falcon ) ? 'yes' : 'no'}` )*/
+var message = "Helpe me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+var episode = 4;
+//episode = '4'
+console.log("This episode: " + episode);
+episode = episode + 1;
+console.log("Next episode: " + episode);
+var favoriteDroid; // tipo any quando não informa o tipo na declaração
+//let favoriteDroid // tipo any quando não informa o tipo na declaração
+favoriteDroid = 'BB-8';
+//favoriteDroid = 10
+console.log('My favorite droid is: ' + favoriteDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 14;
+console.log("is " + distance + " parsecs enough to beat Milenium Falcon? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO') + " ");
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5, 1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
